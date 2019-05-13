333a99d43419ef2718b6a742dc67c5d009a51ea2
陆翊司 <gamedebug@vip.qq.com>
2019-05-13 15:34:53 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import sshclient
 
 if __name__ == "__main__":
-    conn = sshclient.SSHConnection('gamedebug.iok.la', 8202, 'root', '1qaz@WSX3edc$')
+    conn = sshclient.SSHConnection('10.0.0.1', 22, 'root', '123456')
     conn.exec_command('ls -ll')
     #conn.exec_command('cd /root;pwd')  # cd需要特别处理
     #conn.exec_command('pwd')
